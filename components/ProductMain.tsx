4b0c99e2683caefca6e521ef45a0ffffaa9c9a7c
Ronaldo Abreu <ronaldoabreu282@gmail.com>
2026-02-28 21:00:24 +0100

    fix: key ReactNode error en INFO_TABS, usar índice como key

--- components/ProductMain.tsx
+++ components/ProductMain.tsx
@@ -495,8 +495,8 @@ export default function ProductMain() {
                 ))}
               </div>
               <ul className="space-y-2">
-                {INFO_TABS[activeTab].items.map((item) => (
-                  <li key={item} className="flex items-start gap-2.5">
+                {INFO_TABS[activeTab].items.map((item, i) => (
+                  <li key={i} className="flex items-start gap-2.5">
                     <svg className="w-3.5 h-3.5 text-brown-500 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
                     </svg>
